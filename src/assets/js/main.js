ef14c667221773ba826a0d03b74d9d46f1ce325a
Katerina <borodina1902@icloud.com>
2019-10-27 22:58:02 +0200

    fix

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,20 +1,38 @@
-$(document).ready(function() {
-    $(".search").hide();
-    $(".header_search-img-wrap").on("click", function(event){
-        $(".header_addresses").css("display", "none");
-        $(".header_search-img-wrap").css("transform", "translateX(40vw)");
-        $(".search").show().focus().animate({
-        }, function(){
-            $(this).on("blur", function(){
-                $(".search").animate({
-                    "display": "none"
-                }, 100).fadeOut();
-                $(".header_addresses").css("display", "flex");
-                $(".header_search-img-wrap").css("transform", "none");
+$(document).ready(function () {
+    let search = $(".search"),
+        buttonSearch = $(".header_search-img-wrap"),
+        headerAddresses = $(".header_addresses");
+
+
+    search.css({'opacity': '0', 'position': 'absolute', 'z-index': '-10', 'visibility': 'hidden'});
+
+    buttonSearch.on("click", function (event) {
+        headerAddresses.css("opacity", "0");
+
+        setTimeout(function () {
+            headerAddresses.css("position", "absolute");
+            buttonSearch.css("transform", "translateX(40vw)");
+            search.css({'visibility': 'visible', 'position': 'relative', 'z-index': '1', 'opacity': '1'});
+        }, 500);
+
+        setTimeout(function () {
+            search.focus();
+        }, 600);
+
+
+        search.focus().animate({}, function () {
+            $(this).on("blur", function () {
+                search.css({'opacity': '0'});
+                setTimeout(function () {
+                    search.css({'position': 'absolute', 'z-index': '-10', 'visibility': 'hidden'});
+                    headerAddresses.css({'position': 'relative', 'opacity': '1'});
+                }, 500);
+                buttonSearch.css("transform", "translateX(0)");
             });
         });
     });
-    $("form").on("submit", function(e){
+
+    $("form").on("submit", function (e) {
         e.preventDefault();
     });
 
